a0d7d4121e214d5ba97c66c03d4435c79e634a6d
Rob Brander <rob.brander@gmail.com>
2017-02-10 15:55:58 -0500

    Add external links

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>Welcome to rbrander.com</h1>
-    <div className="f3">Where you can learn all about Rob Brander, the developer.</div>
+    <div className="f4">Where you can learn all about Rob Brander, the developer.</div>
+    <ul>
+      <li><a href="https://github.com/rbrander">GitHub</a></li>
+      <li><a href="https://ca.linkedin.com/in/rbrander">LinkedIn</a></li>
+      <li><a href="https://stackoverflow.com/cv/rbrander">StackOverflow CV</a></li>
+      <li><a href="https://stackoverflow.com/story/rbrander">StackOverflow Story</a></li>
+    </ul>
   </div>
 );
 
