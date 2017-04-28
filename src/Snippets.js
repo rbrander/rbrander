2176343432da6e3a67b99d6ade7a34b7c1595d7d
Rob Brander <rob.brander@gmail.com>
2017-04-28 10:53:40 -0400

    Added string repeat polyfill to snippets

--- src/Snippets.js
+++ src/Snippets.js
@@ -6,7 +6,14 @@ const Snippets = () => (
     <h1>Code Snippets</h1>
     <div className="f5">Snippets are small pieces of code that may work on their own; or may be a fragment used for illustration purposes.</div>
     <div className="mt4">
+      <div className="f3">String.prototype.repeat polyfill</div>
+      <div className="pb2"><small className="gray">Created: Apr 28, 2017</small></div>
+      <div className="f5 mb2">A simple polyfill for String.prototype.repeat</div>
+      <EmbeddedGist gist="rbrander/d6c04f96a9a212deec96163ff4e63db5" />
+    </div>
+    <div className="mt4">
       <div className="f3">Fetch</div>
+      <div className="pb2"><small className="gray">Created: Feb 24, 2017</small></div>
       <div className="f5 mb2">A simple HTTP fetch in Node.js using http.get()</div>
       <EmbeddedGist gist="rbrander/73fc675027a70a03a1382c3080c07663" />
     </div>
