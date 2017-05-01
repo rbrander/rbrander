ca4a56dc41c5952f9bdd53c3d33700fd8e26e57c
Rob Brander <rob.brander@gmail.com>
2017-05-01 11:46:25 -0400

    Raise header above content

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Header = () => (
-  <div className="bg-light-blue pa3 flex flex-column flex-row-ns space-between items-center fixed top-0 left-0 right-0">
+  <div className="bg-light-blue pa3 flex flex-column flex-row-ns space-between items-center fixed top-0 left-0 right-0 z-1">
     <Link className="link black ph3" to="/"><strong>rbrander.com</strong></Link>
     <div className="pa2">
       <Link className="link black" to="/About">About</Link>
